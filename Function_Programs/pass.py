c46053a89343da86b2e5dbe5dd7f93ef8e9eccb1
Devansh Kumar Mishra <116156004+Devansh8085@users.noreply.github.com>
2023-12-25 14:05:07 +0530

    Add files via upload

---
+++ Function_Programs/pass.py
@@ -0,0 +1,5 @@
+def dumb_function():
+  pass
+
+print(dumb_function())
+print("No error")
---
+++ Tuple_Programs/ltotuple.py
@@ -0,0 +1,5 @@
+def listToTuple(my_list):
+    # list to tuple
+    print(tuple(my_list))
+
+listToTuple(['Setbacks','makes','us','more','slower!'])
